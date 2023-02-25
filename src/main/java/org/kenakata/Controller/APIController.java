1e767ebf751ff5e18e83483828efd52317d15cbd
remon <remonmizanur@gmail.com>
2023-02-26 00:45:57 +0600

    password hashing, map data

--- src/main/java/org/kenakata/Controller/APIController.java
+++ src/main/java/org/kenakata/Controller/APIController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -70,7 +71,7 @@ public class APIController {
     @PostMapping("/get_users")
     public ResponseEntity<?> getAllUsers() {
         try {
-            HashMap<String, Object> body = new HashMap<String, Object>();
+            LinkedHashMap<String, Object> body = new LinkedHashMap<>(); //hashmap sort its keys, but LinkedHashMap maintain its default order
             body.put("statusCode", HttpStatus.OK.value());
             body.put("data", apiService.getAllUser());
             return ResponseEntity.ok(body);
